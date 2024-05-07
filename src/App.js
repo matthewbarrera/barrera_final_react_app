3ffed22248d6fa8547266055a4a46f108f3eaef5
matthewbarrera <mattbarrera@outlook.com>
2024-05-07 01:40:35 -0400

    setup project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class App extends Component {
+  componentDidMount(){
+  }
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
